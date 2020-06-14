3537974f12c7ac44d1df5a8b0e408da9547fadaf
Kashif Jamal Soofi <kashif.soofi+git@gmail.com>
2020-06-14 16:06:31 +0100

    Run integration tests using testcontainers-dotnet

--- test/TodoApp.Infrastructure.Tests.Integration/DatabaseFixture.cs
+++ test/TodoApp.Infrastructure.Tests.Integration/DatabaseFixture.cs
@@ -1,18 +1,68 @@
 ﻿namespace TodoApp.Infrastructure.Tests.Integration
 {
     using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using TestContainers.Container.Abstractions.Hosting;
+    using TestContainers.Container.Database.Hosting;
+    using TestContainers.Container.Database.MySql;
     using Xunit;
 
-    public class DatabaseFixture : IDisposable
+    public class DatabaseFixture : IDisposable, IAsyncLifetime
     {
+        private readonly MySqlContainer databaseContainer;
+        private MigrationsContainer migrationsContainer;
+
         private bool RunningInDocker => Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER") == "true";
 
         private string Server => RunningInDocker ? "host.docker.internal" : "localhost";
 
-        public string ConnectionString => $"server={Server};Port=33061;database=Todo;uid=root;password=Password123;SslMode=None;";
+        public string ConnectionString => $"Server={this.databaseContainer.GetDockerHostIpAddress()};Port={this.databaseContainer.GetMappedPort(MySqlContainer.DefaultPort)};database=Todo;uid={this.databaseContainer.Username};password={this.databaseContainer.Password};SslMode=None;";
+
+        public DatabaseFixture()
+        {
+            Environment.SetEnvironmentVariable("REAPER_DISABLED", true.ToString());
+            this.databaseContainer = new ContainerBuilder<MySqlContainer>()
+                .ConfigureDockerImageName("mysql:5.6")
+                .ConfigureDatabaseConfiguration("root", "integration123", "defaultdb")
+                .Build();
+        }
 
         public void Dispose()
         {
+        }
+
+        public async Task InitializeAsync()
+        {
+            await this.databaseContainer.StartAsync();
+
+            this.migrationsContainer = new ContainerBuilder<MigrationsContainer>()
+                .ConfigureContainer((context, container) =>
+                {
+                    var connectionString = $"Server=host.docker.internal;Port={this.databaseContainer.GetMappedPort(MySqlContainer.DefaultPort)};database={this.databaseContainer.DatabaseName};uid={this.databaseContainer.Username};password={this.databaseContainer.Password};SslMode=None;";
+                    container.Command = new List<string> { "-cs", connectionString };
+                })
+                .Build();
+
+            await this.migrationsContainer.StartAsync();
+            var exitCode = await this.migrationsContainer.GetExitCodeAsync();
+            if (exitCode > 0)
+            {
+                throw new Exception("Database migrations failed");
+            }
+        }
+
+        public async Task DisposeAsync()
+        {
+            if (this.migrationsContainer != null)
+            {
+                await this.migrationsContainer.StopAsync();
+            }
+
+            if (this.databaseContainer != null)
+            {
+                await this.databaseContainer.StopAsync();
+            }
         }
     }
 
---
+++ test/TodoApp.Infrastructure.Tests.Integration/MigrationsContainer.cs
@@ -0,0 +1,40 @@
+﻿namespace TodoApp.Infrastructure.Tests.Integration
+{
+    using System.Threading.Tasks;
+    using Docker.DotNet;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Logging;
+    using TestContainers.Container.Abstractions;
+    using TestContainers.Container.Abstractions.Hosting;
+    using TestContainers.Container.Abstractions.Images;
+
+    public class MigrationsContainer : GenericContainer
+    {
+        internal IDockerClient DockerClient { get; }
+
+        private static IImage CreateDefaultImage(IDockerClient dockerClient, ILoggerFactory loggerFactory)
+        {
+            return new ImageBuilder<DockerfileImage>()
+                .ConfigureImage((context, image) =>
+                {
+                    image.DockerfilePath = "Dockerfile";
+                    image.DeleteOnExit = true;
+                    image.BasePath = "../../../../../db";
+                })
+                .Build();
+        }
+
+        [ActivatorUtilitiesConstructor]
+        public MigrationsContainer(IDockerClient dockerClient, ILoggerFactory loggerFactory)
+            : base(CreateDefaultImage(dockerClient, loggerFactory), dockerClient, loggerFactory)
+        {
+            this.DockerClient = dockerClient;
+        }
+
+        public async Task<long> GetExitCodeAsync()
+        {
+            var containerWaitResponse = await this.DockerClient.Containers.WaitContainerAsync(this.ContainerId);
+            return containerWaitResponse.StatusCode;
+        }
+    }
+}
